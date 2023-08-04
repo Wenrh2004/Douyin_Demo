2a2da7719f7e01d3573bc3d70d4c7e2ec52889cc
Max Cheng <ofcoursemax@gmail.com>
2023-08-04 15:29:13 -0700

    chore: add kitex_gen.sh for auto generating code using kitex

---
+++ kitex_gen.sh
@@ -0,0 +1,5 @@
+kitex -module Douyin_Demo ./idl/"$1".proto
+
+mkdir service/"$1"
+cd service/"$1"
+kitex -module Douyin_Demo -service "$1" -use Douyin_Demo/kitex_gen/ -I ../../idl/ ../../idl/"$1".proto
