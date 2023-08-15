5f32eba007c1f81b3fea973c111fad7f4ec9abd4
Max Cheng <ofcoursemax@gmail.com>
2023-08-14 21:33:22 -0700

    feat(user): finish basic user handler

--- service/user/main.go
+++ service/user/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"Douyin_Demo/config"
 	user "Douyin_Demo/kitex_gen/douyin/user/userservice"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	consul "github.com/kitex-contrib/registry-consul"
 	"log"
+	"net"
 )
 
 func main() {
-	svr := user.NewServer(new(UserServiceImpl))
+	r, err := consul.NewConsulRegister(config.AppConfig.CONSUL_ADDRESS)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	err := svr.Run()
+	addr, err := net.ResolveTCPAddr("tcp", config.UserServicePort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svr := user.NewServer(new(UserServiceImpl),
+		server.WithRegistry(r),
+		server.WithServiceAddr(addr),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
+			ServiceName: config.UserServiceName,
+		}))
+
+	err = svr.Run()
 
 	if err != nil {
-		log.Println(err.Error())
+		log.Fatal(err.Error())
 	}
 }
