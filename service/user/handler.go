7dcb68000d185c2635b7818bfddbf0f320d5df12
Max Cheng <ofcoursemax@gmail.com>
2023-08-14 20:11:29 -0700

    feat(user): generate code using kitex_gen.sh

---
+++ service/user/handler.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	user "Douyin_Demo/kitex_gen/douyin/user"
+	"context"
+)
+
+// UserServiceImpl implements the last service interface defined in the IDL.
+type UserServiceImpl struct{}
+
+// GetUserInfo implements the UserServiceImpl interface.
+func (s *UserServiceImpl) GetUserInfo(ctx context.Context, req *user.UserInfoRequest) (resp *user.UserInfoResponse, err error) {
+	// TODO: Your code here...
+	return
+}
---
+++ service/user/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	user "Douyin_Demo/kitex_gen/douyin/user/userservice"
+	"log"
+)
+
+func main() {
+	svr := user.NewServer(new(UserServiceImpl))
+
+	err := svr.Run()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
