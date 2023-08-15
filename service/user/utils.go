dda95beada74431765cd5ea3bb0db04332a6a40b
Max Cheng <ofcoursemax@gmail.com>
2023-08-14 21:13:25 -0700

    feat(user): user profile data generator

---
+++ service/user/utils.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"Douyin_Demo/model"
+	"Douyin_Demo/repo"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// TODO integrate with user register
+
+func GenerateUserProfile(userId int64) (profile *model.UserProfile, err error) {
+	avatar := GenerateAvatar(string(userId))
+	signature, err := GenerateSignature()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	background, err := GenerateBackground()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	userP, err := saveUserProfile(userId, avatar, signature, background)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	return userP, nil
+}
+
+// TODO remove db code outside of handler
+func saveUserProfile(userId int64, avatar string, signature string, background string) (userPro *model.UserProfile, err error) {
+	name := "User " + string(userId)
+	userPro = &model.UserProfile{
+		UserId:          userId,
+		Name:            name,
+		Avatar:          avatar,
+		Signature:       signature,
+		BackgroundImage: background,
+	}
+
+	err = repo.Q.UserProfile.Create(userPro)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	return userPro, nil
+}
+
+func GenerateAvatar(userId string) string {
+	return "https://api.multiavatar.com/" + userId + ".png"
+}
+
+func GenerateSignature() (signature string, err error) {
+	url := "https://v1.hitokoto.cn/?encode=text"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "wrong", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: Unexpected status code:", resp.StatusCode)
+		return "wrong", err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "wrong", err
+	}
+
+	return string(body), nil
+}
+
+func GenerateBackground() (imgUrl string, err error) {
+	url := "https://source.unsplash.com/random/1125x633"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: Unexpected status code:", resp.StatusCode)
+		return "", err
+	}
+
+	hotlinkURL := resp.Request.URL.String()
+	return hotlinkURL, nil
+}
